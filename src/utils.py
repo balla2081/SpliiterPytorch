d2cf70b1c0258a46c5be1aefa73b8215c0dc259c
JisungYoon <balla2081@postech.ac.kr>
2019-07-26 07:34:25 +0900

    edit util

--- src/utils.py
+++ src/utils.py
@@ -3,29 +3,29 @@ import networkx as nx
 from texttable import Texttable
 
 def tab_printer(args):
-    """
-    Function to print the logs in a nice tabular format.
-    :param args: Parameters used for the model.
-    """
-    args = vars(args)
-    keys = sorted(args.keys())
-    t = Texttable() 
-    t.add_rows([["Parameter", "Value"]] +  [[k.replace("_"," ").capitalize(),args[k]] for k in keys])
-    print(t.draw())
+	"""
+	Function to print the logs in a nice tabular format.
+	:param args: Parameters used for the model.
+	"""
+	args = vars(args)
+	keys = sorted(args.keys())
+	t = Texttable() 
+	t.add_rows([["Parameter", "Value"]] +  [[k.replace("_"," ").capitalize(),args[k]] for k in keys])
+	print(t.draw())
 
 def read_graph(input_file_path, weighted=False, directed=False, ):
-		'''
-		Reads the input network in networkx.
-		'''
-		if weighted:
-				G = nx.read_edgelist(input_file_path, nodetype=str, data=(('weight',float),), create_using=nx.DiGraph())	
+	'''
+	Reads the input network in networkx.
+	'''
+	if weighted:
+		G = nx.read_edgelist(input_file_path, nodetype=str, data=(('weight',float),), create_using=nx.DiGraph())	
 		
-		else:
-				G = nx.read_edgelist(input_file_path, nodetype=str, create_using=nx.DiGraph())
-				for edge in G.edges():
-						G[edge[0]][edge[1]]['weight'] = 1
+	else:
+		G = nx.read_edgelist(input_file_path, nodetype=str, create_using=nx.DiGraph())
+		for edge in G.edges():
+			G[edge[0]][edge[1]]['weight'] = 1
 
-		if not directed:
-				G = G.to_undirected()
+	if not directed:
+		G = G.to_undirected()
 
-		return G
+	return G
